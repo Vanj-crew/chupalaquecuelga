1aae710942c3620fd69b641bc28aa1db60f69e8f
Retriman <nicolas_katre@hotmail.com>
2011-05-07 13:26:18 -0400

    Revert "Añadido Wintergrasp"
    This reverts commit 6ae3f26ba171ac1c0d572a3d0170c4739681dc47.
    
    Conflicts:
    
    	sql/Madobox_Update/Gral/Wintergrasp.sql
    	src/server/game/Entities/GameObject/GameObject.cpp

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -595,8 +595,6 @@ void AddSC_outdoorpvp_na();
 void AddSC_outdoorpvp_si();
 void AddSC_outdoorpvp_tf();
 void AddSC_outdoorpvp_zm();
-void AddSC_outdoorpvp_wg();
-void AddSC_wintergrasp();
 
 // player
 void AddSC_chat_log();
@@ -1211,7 +1209,6 @@ void AddNorthrendScripts()
     AddSC_zuldrak();
     AddSC_crystalsong_forest();
     AddSC_isle_of_conquest();
-    AddSC_wintergrasp();
 #endif
 }
 
@@ -1224,7 +1221,6 @@ void AddOutdoorPvPScripts()
     AddSC_outdoorpvp_si();
     AddSC_outdoorpvp_tf();
     AddSC_outdoorpvp_zm();
-    AddSC_outdoorpvp_wg();
 #endif
 }
 
