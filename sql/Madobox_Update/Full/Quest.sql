fcf2eb6ebcdc626d54c10df8724086a393816419
Barneysk8 <adrian_mardones@hotmail.com>
2011-05-23 01:00:18 -0400

    [Quest][10866] a Full

--- sql/Madobox_Update/Full/Quest.sql
+++ sql/Madobox_Update/Full/Quest.sql
@@ -597,3 +597,6 @@ VALUES
 -- Fix for Quest 12066 The Focus on the Beach
 DELETE FROM `spell_linked_spell` WHERE `spell_trigger` = 47374; 
 INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `comment`) VALUES (47374, 47391, 'Ley Line Information');
+
+-- Fix quest 10866 - Zuluhed the Whacked /`ReqCreatureOrGOId1` 22112
+UPDATE `quest_template` SET `ReqCreatureOrGOId1` = -185156, `ObjectiveText1` = 'Karynaku freed', `ReqCreatureOrGOId2` = 11980 , `ObjectiveText2` = 'Zuluhed the Whacked slain'  WHERE entry = 10866;
