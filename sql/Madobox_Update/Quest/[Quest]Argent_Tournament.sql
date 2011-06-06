138223f84fd41df7470232e9ca36e44e93441d6a
Retriman <nicolas_katre@hotmail.com>
2011-06-05 21:27:27 -0400

    Script/ArgentTournament: Fix bug npc's Las quest que otorgan los NPC "High crusader Adelard" http://old.wowhead.com/npc=34882 y el NPC "Crusader Silverdawn" http://old.wowhead.com/npc=35094 Ahora solo daran 1.

--- sql/Madobox_Update/Quest/[Quest]Argent_Tournament.sql
+++ sql/Madobox_Update/Quest/[Quest]Argent_Tournament.sql
@@ -5,3 +5,16 @@ INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 ('62544', 'spell_tournament_melee'),
 ('62863', 'spell_tournament_duel');
 
+-- Fix Quest Daily solo tomar 1 al dia.
+-- Crusader dailies
+-- Mistcaller Yngvar
+-- Drottinn Hrothgar
+-- Ornolf The Scarred
+-- Deathspeaker Kharos
+UPDATE `quest_template` SET `ExclusiveGroup`=14102, `NextQuestId`=0, `NextQuestInChain`=0 WHERE `entry` IN (14102, 14101, 14104, 14105);
+-- The Fate Of The Fallen
+-- Get Kraken!
+-- Identifying the Remains
+UPDATE `quest_template` SET `ExclusiveGroup`=14107, `NextQuestId`=0, `NextQuestInChain`=0 WHERE `entry` IN (14107, 14108, 14095);
+
+
