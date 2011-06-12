4d1902383406d7cb87e4b2b4daef3a6a6a5901e5
Retriman <nicolas_katre@hotmail.com>
2011-06-12 14:11:58 -0400

    Fix Drak'Tharon Keep reward for DF.

--- sql/Madobox_Update/Dungeon/[Dungeon][Wotlk]Drak'Tharon.sql
+++ sql/Madobox_Update/Dungeon/[Dungeon][Wotlk]Drak'Tharon.sql
@@ -1,2 +1,5 @@
 -- Make Drakkari Guardian ignoring Aggro
 UPDATE creature_template SET unit_flags = unit_flags | 2 WHERE entry IN (26620,31339);
+
+-- Fix Drak'Tharon Keep reward for DF.
+UPDATE `instance_encounters` SET `creditType` = '0', `creditEntry` = '26632' WHERE `entry` IN ('376', '375');
