aabc1153464702c9edda5499d6e25e4b9b0f1d58
Retriman <nicolas_katre@hotmail.com>
2011-06-19 15:38:03 -0400

    Argent Tournament: Quest At The Enemy's Gates añadido npc Boneguard Lieutenant faltante

--- sql/Madobox_Update/Quest/[Quest]Argent_Tournament.sql
+++ sql/Madobox_Update/Quest/[Quest]Argent_Tournament.sql
@@ -494,3 +494,24 @@ INSERT INTO `creature` (`id`, `map`, `spawnMask`, `phaseMask`, `modelid`, `equip
 ('33499','571','1','1','0','1858','5443.25','371.767','163.366','4.35449','300','5','0','12175','0','0','1');
 
 
+
+SET @Guid=1000141; -- SELECT creature.guid FROM creature ORDER BY creature.guid DESC LIMIT 1;
+DELETE FROM `creature` WHERE id=33429;
+INSERT INTO `creature` (guid,id,map,spawnMask,phaseMask,modelid,equipment_id,position_x,position_y,position_z,orientation,spawntimesecs,spawndist,currentwaypoint,curhealth,curmana,DeathState,MovementType,npcflag,unit_flags,dynamicflags) VALUES
+(@Guid+0,33429,571,1,256,0,0,6253.47,2263.21,244.456,0.488692,120,0,0,1,0,0,0,0,0,0),
+(@Guid+1,33429,571,1,256,0,0,6217.16,2252.59,496.038,0.488692,120,0,0,1,0,0,0,0,0,0),
+(@Guid+2,33429,571,1,256,0,0,6234.1,2301.56,488.447,0.488692,120,0,0,1,0,0,0,0,0,0),
+(@Guid+3,33429,571,1,256,0,0,6163.18,2231.77,506.981,0.488692,120,0,0,1,0,0,0,0,0,0),
+(@Guid+4,33429,571,1,256,0,0,6172.18,2257.27,503.146,0.488692,120,0,0,1,0,0,0,0,0,0),
+(@Guid+5,33429,571,1,256,0,0,6242.42,2246.47,491.941,0.56266,120,0,0,1,0,0,0,0,0,0),
+(@Guid+6,33429,571,1,256,0,0,6202.35,2284.08,495.286,0.488692,120,0,0,1,0,0,0,0,0,0),
+(@Guid+7,33429,571,1,256,0,0,6145.68,2208.17,512.426,0.488692,120,0,0,1,0,0,0,0,0,0);
+
+-- Template updates for creature 33429 (Boneguard Lieutenant)
+UPDATE `creature_template` SET speed_run=2 WHERE entry=33429; -- Boneguard Lieutenant
+-- Model data 29098 (creature 33429 (Boneguard Lieutenant))
+UPDATE creature_model_info SET bounding_radius=0.459,combat_reach=2.25,gender=0 WHERE modelid=29098; -- Boneguard Lieutenant
+-- Addon data for creature 33429 (Boneguard Lieutenant)
+DELETE FROM creature_template_addon WHERE entry=33429;
+INSERT INTO creature_template_addon (entry,mount,bytes1,bytes2,emote,auras) VALUES
+(33429,25678,0,1,0,NULL); -- Boneguard Lieutenant
