a4c6520d16e0d6c82fa969ec343ed7f7b5696e31
svdarthock <justsvdiablo@live.cl>
2011-06-25 12:05:30 -0700

    [FIX][DK]Spell Maquina de matar (Frecuencia acorde al punto de talento asignado)

--- sql/Madobox_Update/Spells/scent_of_blood.sql
+++ sql/Madobox_Update/Spells/scent_of_blood.sql
@@ -2,3 +2,10 @@ INSERT INTO `spell_proc_event` (`entry`,`procEx`) VALUES
 (49004,0x00000033),
 (49508,0x00000033),
 (49509,0x00000033);
+
+-- Añadida correccion frecuencia "Maquina de Matar"
+UPDATE spell_proc_event SET ppmRate = 1, procEx = 1 WHERE entry = 51123; 
+UPDATE spell_proc_event SET ppmRate = 2, procEx = 1 WHERE entry = 51127; 
+UPDATE spell_proc_event SET ppmRate = 3, procEx = 1 WHERE entry = 51128; 
+UPDATE spell_proc_event SET ppmRate = 4, procEx = 1 WHERE entry = 51129; 
+UPDATE spell_proc_event SET ppmRate = 5, procEx = 1 WHERE entry = 51130;
